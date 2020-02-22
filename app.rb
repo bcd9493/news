453b966733602e5d0b382a2e6739c963c0cb1d0a
bcd9493 <brad.dominy@kellogg.northwestern.edu>
2020-02-22 17:01:35 +0000

    another

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ get "/news" do
 
 results = Geocoder.search(params["q"])
     lat_long = results.first.coordinates # => [lat, long]
-    
+    location = results.first.city
+
 # Define the lat and long
 lat = "#{lat_long [0]}"
 long = "#{lat_long [1]}"
@@ -32,8 +33,17 @@ long = "#{lat_long [1]}"
 # Results from Geocoder
 forecast = ForecastIO.forecast("#{lat}" , "#{long}").to_hash
 
-#News
-url = "http://newsapi.org/v2/top-headlines?q=&apiKey=72062d412df44b118b2b3128033cf057"
-news = HTTParty.get(url).parsed_response.to_hash
+current_temperature = forecast["currently"]["temperature"]
+conditions = forecast["currently"]["summary"]
+
+puts "In #{location}, it is currently #{current_temperature} and #{conditions}"
+# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+# puts high_temperature
+# puts forecast["daily"]["data"][1]["temperatureHigh"]
+# puts forecast["daily"]["data"][2]["temperatureHigh"]
+
+for day_forecast in forecast["daily"]["data"]
+  puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
+end
 
 end
