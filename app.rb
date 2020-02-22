e06936bc97b2b72ba711ac9463ffe5e5f0936207
bcd9493 <brad.dominy@kellogg.northwestern.edu>
2020-02-22 16:36:51 +0000

    Push

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ require "httparty"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
+
 # enter your Dark Sky API key here
 ForecastIO.api_key = "9e7da56190cbaf7f50b97dda1c4adcdd"
-forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 
 # News API
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=72062d412df44b118b2b3128033cf057"
@@ -21,9 +21,19 @@ end
 
 get "/news" do
   # do everything else
-  results = Geocoder.search(params["q"])
-    lat_long = results.first.coordinates # => [lat, long]
-    "#{lat_long[0]} #{lat_long[1]}"
 
-  
+results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+    
+# Define the lat and long
+lat = "#{lat_long [0]}"
+long = "#{lat_long [1]}"
+
+# Results from Geocoder
+forecast = ForecastIO.forecast("#{lat}" , "#{long}").to_hash
+
+#News
+url = "http://newsapi.org/v2/top-headlines?q=&apiKey=72062d412df44b118b2b3128033cf057"
+news = HTTParty.get(url).parsed_response.to_hash
+
 end
